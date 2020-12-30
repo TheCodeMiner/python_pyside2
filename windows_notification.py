8dbd8602ac5016064642380445062a8afcd7a947
Sam Mobasery <42114472+TheCodeMiner@users.noreply.github.com>
2020-12-30 15:05:27 +0100

    Update windows_notification.py

--- windows_notification.py
+++ windows_notification.py
@@ -4,15 +4,15 @@ import time
 from win10toast import ToastNotifier
 
 
-def timer(message, message_duration, delay):
+def notifier(title, message, message_duration, delay):
     notificator = ToastNotifier()
-    notificator.show_toast(title="Achtung", msg=message, duration=message_duration)
+    # set threaded=True if you want to execute other part of code to rune while this notification is active
+    notificator.show_toast(title, message, duration=message_duration, threaded=False)
     time.sleep(delay)
 
 
 if __name__ == "__main__":
-    message_to_show = "Leave your desk and do some body exercises"
-    show_message_time = 3   # show message for this much seconds
-    delay_until_next_popup = 5     # after message has been show, wait this much seconds and repeate
+    show_message_time = 4   # show message for this much seconds
+    delay_until_next_popup = 6     # after message has been show, wait this much seconds and repeate
     while True:
-        timer(message_to_show, show_message_time, delay_until_next_popup)
+        notifier("Notification title", "Your Message", show_message_time, delay_until_next_popup)
